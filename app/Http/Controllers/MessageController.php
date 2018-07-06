ba0e0b5572bf7e8598048b7a633899a875a1c37f
Ozberk Ercin <ozberk@gmail.com>
2018-07-06 08:39:04 +0200

    add listing params

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,26 +13,26 @@ class MessageController extends Controller
      *   path="/list",
      *   summary="Gets list of messages",
      *   @SWG\Parameter(
-     *     	    name="status",
-     * 			in="query",
-     * 			required=false,
-     * 			type="string",
-     * 			description="Read status",
+     *            name="status",
+     *            in="query",
+     *            required=false,
+     *            type="string",
+     *            description="Read status",
      *          enum={"read", "unread"}
      *  ),
      *   @SWG\Parameter(
-     *     	    name="offset",
-     * 			in="query",
-     * 			required=true,
-     * 			type="integer",
-     * 			description="From"
+     *            name="offset",
+     *            in="query",
+     *            required=true,
+     *            type="integer",
+     *            description="From"
      *  ),
      *   @SWG\Parameter(
-     *     	    name="limit",
-     * 			in="query",
-     * 			required=true,
-     * 			type="integer",
-     * 			description="How many records should be returned"
+     *            name="limit",
+     *            in="query",
+     *            required=true,
+     *            type="integer",
+     *            description="How many records should be returned"
      *  ),
      *   @SWG\Response(response=200, description="successful operation")
      * )
@@ -43,8 +43,22 @@ class MessageController extends Controller
      */
     public function index(Request $request)
     {
-        $messages = Message::all();
+        $this->validate($request, [
+            'status' => 'in:' . Message::STATUS_READ . ',' . Message::STATUS_UNREAD,
+            'offset' => 'required|numeric',
+            'limit' => 'required|numeric',
+        ]);
 
-        return $messages;
+        $status = $request->get("status");
+        $offset = (int)$request->get("offset");
+        $limit = (int)$request->get("limit");
+
+        if ($status) {
+            $collection = Message::where('status', '=', $status)->skip($offset)->take($limit)->get();
+        } else {
+            $collection = Message::skip($offset)->take($limit)->get();
+        }
+
+        return $collection;
     }
 }
